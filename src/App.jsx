3ca863b1e24aa0bdd57d7a04003b309102cbc982
Jeferson Laurentino <128257769+jefersonlaurentino@users.noreply.github.com>
2024-08-28 17:03:18 -0300

    correção de bugs

--- src/App.jsx
+++ src/App.jsx
@@ -66,15 +66,14 @@ export function App() {
 
   useEffect(() => {
     const suaLocalizacao = navigator.geolocation.watchPosition((localizacao) => {
-      console.log(localizacao)
       apiLocation(localizacao.coords.latitude,localizacao.coords.longitude)
     }, function(erro) {
       console.log(erro);
       document.querySelector(".info_city").innerHTML = "Permissão de Localização Bloqueada."
     }, { enableHighAccuracy: true , maximumAge: 30000, timeout: 30000})
-    // setTimeout(() => {
-    //   (navigator.geolocation.clearWatch(suaLocalizacao))
-    // }, 6000);
+    setTimeout(() => {
+      (navigator.geolocation.clearWatch(suaLocalizacao))
+    }, 6000);
   },[])
 
 
